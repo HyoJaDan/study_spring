ff55ade3d4239784a3a9c0fcd203454ee43f4a47
HyoJaDan <kopasd99@gmail.com>
2024-11-01 16:51:35 +0900

    회원가입 로직 구현

--- 7.spring_security/1.springJWT/src/main/java/com/Hyoja1/springJWT/Repository/UserRepository.java
+++ 7.spring_security/1.springJWT/src/main/java/com/Hyoja1/springJWT/Repository/UserRepository.java
@@ -4,5 +4,5 @@ import com.Hyoja1.springJWT.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+    Boolean existsByUsername(String username);
 }
