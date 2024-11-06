eceb1f66d357533abc9a180e38023d693b10a672
전성호 <kopasd99@gmail.com>
2024-11-06 11:28:43 +0900

    다중 (Refresh/Access) 토큰 발급

--- 7.spring_security/2.AccessRefresh/src/main/java/com/example/springjwt/config/SecurityConfig.java
+++ 7.spring_security/2.AccessRefresh/src/main/java/com/example/springjwt/config/SecurityConfig.java
@@ -35,7 +35,6 @@ public class SecurityConfig {
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
-
         return new BCryptPasswordEncoder();
     }
 
--- 7.spring_security/2.AccessRefresh/src/main/java/com/example/springjwt/service/CustomUserDetailsService.java
+++ 7.spring_security/2.AccessRefresh/src/main/java/com/example/springjwt/service/CustomUserDetailsService.java
@@ -10,25 +10,17 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
-
     private final UserRepository userRepository;
-
     public CustomUserDetailsService(UserRepository userRepository) {
-
         this.userRepository = userRepository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
         UserEntity userData = userRepository.findByUsername(username);
-
         if (userData != null) {
-
             return new CustomUserDetails(userData);
         }
-
-
         return null;
     }
 }
