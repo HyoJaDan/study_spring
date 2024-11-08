7c55c28a8f7e40018e132a31ab7c48966aa2999f
HyoJaDan <kopasd99@gmail.com>
2024-11-08 15:06:38 +0900

    DB 연결

--- 7.spring_security/3.OAuthSession/3.OAuthSession/src/main/java/com/OAuthSession3/OAuthSession/service/CustomOAuth2UserService.java
+++ 7.spring_security/3.OAuthSession/3.OAuthSession/src/main/java/com/OAuthSession3/OAuthSession/service/CustomOAuth2UserService.java
@@ -1,9 +1,12 @@
 package com.OAuthSession3.OAuthSession.service;
 
+import com.OAuthSession3.OAuthSession.domain.UserEntity;
 import com.OAuthSession3.OAuthSession.dto.CustomOAuth2User;
 import com.OAuthSession3.OAuthSession.dto.GoogleResponse;
 import com.OAuthSession3.OAuthSession.dto.NaverResponse;
 import com.OAuthSession3.OAuthSession.dto.OAuth2Response;
+import com.OAuthSession3.OAuthSession.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -11,7 +14,10 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+    private final UserRepository userRepository;
+
     //DefaultOAuth2UserService OAuth2UserService의 구현체
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -33,6 +39,42 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
             oAuth2Response = new GoogleResponse(oAuth2User.getAttributes());
         }
         else return null;
+
+        // 1. 어느 소셜 로그인(google | naver)에서 받았는지 확인하는 getProvider, user에 대한 번호 데이터 ID를 받음
+        String username = oAuth2Response.getProvider()+" "+oAuth2Response.getProviderId();
+        // 2. 1번에서 받은 id를 가지고 repository에서 데이터 확인
+        UserEntity existData = userRepository.findByUsername(username);
+
+        // 3. 처음 회원가입한 유저라면
+        if (existData == null) {
+            UserEntity userEntity = new UserEntity();
+            userEntity.setUsername(username);
+            userEntity.setEmail(oAuth2Response.getEmail());
+            userEntity.setName(oAuth2Response.getName());
+            userEntity.setRole("ROLE_USER");
+
+            userRepository.save(userEntity);
+
+//            UserDTO userDTO = new UserDTO();
+//            userDTO.setUsername(username);
+//            userDTO.setName(oAuth2Response.getName());
+//            userDTO.setRole("ROLE_USER");
+//
+//            return new CustomOAuth2User(userDTO);
+        }
+        else {
+            existData.setEmail(oAuth2Response.getEmail());
+            existData.setName(oAuth2Response.getName());
+
+            userRepository.save(existData);
+
+//            UserDTO userDTO = new UserDTO();
+//            userDTO.setUsername(existData.getUsername());
+//            userDTO.setName(oAuth2Response.getName());
+//            userDTO.setRole(existData.getRole());
+//
+//            return new CustomOAuth2User(userDTO);
+        }
 
         String role = "ROLE_USER";
         return new CustomOAuth2User(oAuth2Response, role);
